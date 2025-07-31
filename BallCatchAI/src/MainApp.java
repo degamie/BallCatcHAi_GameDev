0ad3c21fb3f5ebcf681ac78f5f31691ce6838236
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-01 00:20:53 +0530

    Add files via upload

---
+++ BallCatchAI/src/MainApp.java
@@ -0,0 +1,7 @@
+public class MainApp {
+    Background bg=new Background();
+    bg.setColor(.0f,.7;1.5f);
+    bg.setApplicationContext;
+    bg.addChilden
+    bg.addChild(bg);                                                                                                                                                                                                                                                                                                                                  wwwwwwwwwwwwwwwwwwwwwwwww)
+})
