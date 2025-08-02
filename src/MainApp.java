3535130e9360a49f91c75bc0dd93ff09ecfa461f
Degamie_sign <degamiesign@gmail.com>
2025-08-02 16:59:48 +0530

    Initial Commit

---
+++ src/MainApp.java
@@ -0,0 +1,18 @@
+
+import java.applet.Applet;
+import java.awt.GraphicsConfiguration;
+
+public class MainApp extends Applet implements KeyListener {
+    public Matrix matrix=new Matrix(); 
+    Background bg=new Background();
+    bg.setColor(.0f,.7;1.5f);
+    bg.setApplicationContext;
+    bg.addChilden
+    bg.addChild(bg);       
+    
+    public Canvas3d(GraphicsConfiguration GraphicsConfiguration,String OffScreen){
+        this.GraphicsConfiguration=GraphicsConfiguration;
+        this.OffScreen=OffScreen;//Binding OffScreen And GraphicsConfigura
+    }
+
+}
