fce6c807c12feebdbdb93023510caa20c4a0ef01
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-03 22:00:25 +0530

    Add files via upload

--- BallCatchAI/src/MainApp.java
+++ BallCatchAI/src/MainApp.java
@@ -1,15 +1,30 @@
 
+import java.applet.Applet;
 import java.awt.GraphicsConfiguration;
 
-public class MainApp {
-    Background bg=new Background();
-    bg.setColor(.0f,.7;1.5f);
-    bg.setApplicationContext;
-    bg.addChilden
-    bg.addChild(bg);       
-    
-    public Canvas3d(GraphicsConfiguration GraphicsConfiguration,String OffScreen){
-        this.GraphicsConfiguration=GraphicsConfiguration;
-        this.OffScreen=OffScreen;//Binding OffScreen And GraphicsConfigura
+public class MainApp extends Applet implements KeyListener {//inheriting Applet and KeyListener Class
+    public BatchGroup Rootobj=new BatchGroup();
+    public TransformGroup transformGroup=new TransformGroup();
+    public Matrix matrix=new Matrix(); //Matrix Obj Declare
+    Background bg=new Background();//Background Obj declare
+    bg.setColor(.0f,.7;1.5f);//Setting up Background's Col
+    bg.setApplicationContext;//Setting Up application
+    bg.addChilden;//Adding Background's Child Nodes
+    bg.addChild(bg); //Adding  Background's Child
+    public BatchGroup createBallAI(){//createBallAI Method Declare
+        BatchGroup Rootobj=new BatchGroup();//BatchGroup Obj declare
+        TransformGroup transformGroup=new TransformGroup();//transformGroup Declare
+        public Scene scene=new Scene("mdl/Ball.fbx");//Scene 's Fbx Model Declare
     }
+    public BatchGroup createSceneGraph(){//createSceneGraph Method Declare
+        BatchGroup BatchGroup=new BoundingSphere(new Points3d(),1500.0);//BatchGroup Obj Declare
+        ViewTrans ViewTrans=universe.getViewingPlatform.getView('Platform transform');//ViewTrans Obj Declare
+    }      
+    
+    public Canvas3d(GraphicsConfiguration GraphicsConfiguration,String OffScreen){//Canvas3d Param Constructor
+        this.GraphicsConfiguration=GraphicsConfiguration;//Binding GraphicsConfiguration Node
+        this.OffScreen=OffScreen;//Binding OffScreen Node
+    }
+
+    
 }
