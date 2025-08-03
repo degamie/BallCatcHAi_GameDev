43cdb58d395d596242d693e42d493503c16df799
Degamie_sign <degamiesign@gmail.com>
2025-08-03 21:59:30 +0530

    1st Next Commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,13 +3,19 @@ import java.applet.Applet;
 import java.awt.GraphicsConfiguration;
 
 public class MainApp extends Applet implements KeyListener {//inheriting Applet and KeyListener Class
+    public BatchGroup Rootobj=new BatchGroup();
+    public TransformGroup transformGroup=new TransformGroup();
     public Matrix matrix=new Matrix(); //Matrix Obj Declare
     Background bg=new Background();//Background Obj declare
     bg.setColor(.0f,.7;1.5f);//Setting up Background's Col
     bg.setApplicationContext;//Setting Up application
     bg.addChilden;//Adding Background's Child Nodes
     bg.addChild(bg); //Adding  Background's Child
-
+    public BatchGroup createBallAI(){//createBallAI Method Declare
+        BatchGroup Rootobj=new BatchGroup();//BatchGroup Obj declare
+        TransformGroup transformGroup=new TransformGroup();//transformGroup Declare
+        public Scene scene=new Scene("mdl/Ball.fbx");//Scene 's Fbx Model Declare
+    }
     public BatchGroup createSceneGraph(){//createSceneGraph Method Declare
         BatchGroup BatchGroup=new BoundingSphere(new Points3d(),1500.0);//BatchGroup Obj Declare
         ViewTrans ViewTrans=universe.getViewingPlatform.getView('Platform transform');//ViewTrans Obj Declare
@@ -19,5 +25,6 @@ public class MainApp extends Applet implements KeyListener {//inheriting Applet 
         this.GraphicsConfiguration=GraphicsConfiguration;//Binding GraphicsConfiguration Node
         this.OffScreen=OffScreen;//Binding OffScreen Node
     }
+
     
 }
