04600bdcfff93645a149243895444d87f4ee8250
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-31 14:43:46 +0530

    Add files via upload

--- src/MainMenu/MainMenuScreen.java
+++ src/MainMenu/MainMenuScreen.java
@@ -1,34 +1,54 @@
-//WID(30/01/2026)(Sarthak Mittal)(degamieSign)#1.1.1.1
-
-public class MainMenuScreen extends SimpleApplication{
-    public MainMenuScreen menuScreen;
-    public void setMenuScreen(MainMenuScreen menuScreen){this.menuScreen=menuScreen;}//Binding MenuScreen in App
-    public MainMenuScreen getMenuScreen(MainMenuScreen menuScreen){return menuScreen;}
+//WID(31/1/2026)(DegamieSign)(Sarthak Mittal)#1
+import javax.swing.event.MenuEvent;
+    public class MainMenuScreen extends SimpleApplication{
+    MainMenuScreen menuScreen=new MainMenuScreen();
     public String MainMenu(String[] args){
         MenuBar menuBar=new MenuBar();
         FileMenu menu=new FileMenu();
         FileMenu openMenu=new FileMenu("openMenu");
         FileMenu SaveMenu=new FileMenu("SaveMenu");
         FileMenu exitMenu=new FileMenu("exitMenu");
-        public   FileMenu getExit(FileMenu exitMenu){return exitMenu;}//Fethcing ExitMenu in App
-        public void setOpenMenu(FileMenu openMenu){this.openMenu=openMenu;}//Binding OpenMenu in App
-        public FileMenu getMenu(FileMenu menu){return menu;}//Fetching Menu in App
-        public FileMenu getSaveMenu(FileMenu saveMenu){return saveMenu;}//Fetching SaveMenu in App
-        public void setsaveMenu(FileMenu SaveMenu){return SaveMenu;}//Binding SaveMenu in App
-        public FileMenu getOpenMenu(FileMenu openMenu){return openMenu;}//Fetching OpenMenu in App
-        public Strng getmenu(FileMenu menu){return menu;}
-        public void setMenu(FileMenu openMenu){this.openmenu=openMenu;}//Binding OpenMenu in App
+        public MenuBar getMenuBar(MenuBar menubar){return menuBar;}//Fetching MenuBar in App
+        public FileMenu getexitMenu(exitMenu){return exitMenu;}//Fetching exitMenu in App
+        public String getexitMenu(FileMenu exitMenu){return exitMenu;}//Fetching exitMenu
+        public void setexitMenu(FileMenu exitMenu){this.exitMenu=exitMenu;}//Bidning  ExitMenu in App
+        public String getopenMenu(FileMenu openMenu){return  openMenu;}//Fetching OpenMenu
+    public String getMenuBar(MenuBar menubar){return menuBar;}//Fetching MenuBar in App
+        public String getMenu(MenuBar menuBar){return menuBar;}//Fetching menuBar
+        public String getSaveMenu(FileMenu SaveMenu){return SaveMenu;}//Fetching Save menu
+        public String updateByMenu(FileMenu menu,MenuBar menuBar){getMenu(menuBar)+setmenu(menuBar)+1;}//updating Menu in App
+        public void setSaveMenu(FileMenu saveMenu){this.savemenu=SaveMenu;}//Binding SaveMemu in Server
+        public String updateAllbySaveMenu(FileMenu saveMenu){getSaveMenu(saveMenu)+set(SaveMenu)+1;}//UPDATING Save M
+        public String updateByOpenMenu(String OpenMenu){getopenMenu(openMenu)+setOpenmenu(openMenu)+1;}//Updating OpenMenu in Server
+        public String setMenu(MenuBar menuBar){this.menuBar=menuBar;}//Binding MenuBar
+        filemenu.getItems().addAll(openMenu,SaveMenu,exitMenu);
+        MainMenuScreen(MenuBar menuBar){
+            this.menuBar=menuBar;
+        }
+        public String updateByMenu(Menubar menu){getMenu(menu)+setMenu(menu)+1;}//Up[dating Menu in App
 
-            filemenu.getItems().addAll(openMenu,SaveMenu,exitMenu);
-        public String getOpenMenu(String openMenu){return openMenu;}
-    }
-    public String EditMenu(String[] args){
+        public MenuEvent onMenuStrart(UserInput userInp,MenuBar menuBar,String[] path){
+            while(userInp!=notifyAll()){   
+                    if(!userInp.KEYBOARD_RIGHT_CLICK==null ||!userInp.KEYBOARD_LEFT_CLICK==null )openMenu.show();
+                    else if(!userInp.KEYBOARD_RIGHT_CLICK==null ||!userInp.KEYBOARD_LEFT_CLICK==null )SaveMenu.show();
+                    else if(!userInp.KEYBOARD_RIGHT_CLICK==null ||!userInp.KEYBOARD_LEFT_CLICK==null )exitMenu.show();
+                    else menuBar.hide();}
+     
+         return menuBar;    
+
+        }
+
+    }//     menuBar.getMenus().addAll(filemenu,editMenu,viewMenu,helpMenu);
+          
+    public String EditMenu(String[] args,EditMenu editMenu,EditMenu soundMenu,EditMenu VideoMenu,EditMenu OptionsMenu){
         EditMenu editMenu=new EditMenu();
-
         EditMenu soundMenu=new FileMenu("soundMenu");
         EditMenu VideoMenu=new FileMenu("=VideoMenu");
         EditMenu OptionsMenu=new FileMenu("OptionsMenu");
 
         filemenu.getItems().addAll(editMenu,soundMenu,VideoMenu,OptionsMenu);
+    }
+    public String getEditMenu(String[] args,EditMenu editMenu,EditMenu soundMenu,EditMenu VideoMenu,EditMenu OptionsMenu){
+        EditMenu(args,editMenu,soundMenu,VideoMenu,OptionsMenu);
     }
 }
