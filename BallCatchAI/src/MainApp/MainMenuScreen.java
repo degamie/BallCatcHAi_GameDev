865d7b44d2aa9ff35934f87349329a46e0e77e03
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-07 21:19:04 +0530

    Add files via upload

--- BallCatchAI/src/MainApp/MainMenuScreen.java
+++ BallCatchAI/src/MainApp/MainMenuScreen.java
@@ -1,12 +1,29 @@
-//WID(26/01/2026)(Sarthak Mittal)(degamieSign)#1.1
+//WID(7/02/2026)(Sarthak Mittal)(degamieSign)#1.1.1.1.1,1
+
 public class MainMenuScreen extends SimpleApplication{
     public MainMenuScreen menuScreen;
+    public void updateByMenuScreen(MainMenuScreen menuScreen){getMenuScreen(menuScreen)+setMenuScreen(menuScreen)+1;}//Updating MenuScreen in App
+    public void setMenuScreen(MainMenuScreen menuScreen){this.menuScreen=menuScreen;}//Binding MenuScreen in App
+    public MainMenuScreen getMenuScreen(MainMenuScreen menuScreen){return menuScreen;}
+    public String EditMenu(String[] args){
+        EditMenu editMenu=new EditMenu();
+        EditMenu soundMenu=new FileMenu("soundMenu");
+        EditMenu VideoMenu=new FileMenu("=VideoMenu");
+        EditMenu OptionsMenu=new FileMenu("OptionsMenu");
+        public EditMenu getOptionsMenu(EditMenu opionsMenu){
+             return opionsMenu;
+        }
+
+        filemenu.getItems().addAll(editMenu,soundMenu,VideoMenu,OptionsMenu);
+    }
     public String MainMenu(String[] args){
         MenuBar menuBar=new MenuBar();
         FileMenu menu=new FileMenu();
         FileMenu openMenu=new FileMenu("openMenu");
         FileMenu SaveMenu=new FileMenu("SaveMenu");
         FileMenu exitMenu=new FileMenu("exitMenu");
+        public   FileMenu getExit(FileMenu exitMenu){return exitMenu;}//Fethcing ExitMenu in App
+        public void setOpenMenu(FileMenu openMenu){this.openMenu=openMenu;}//Binding OpenMenu in App
         public FileMenu getMenu(FileMenu menu){return menu;}//Fetching Menu in App
         public FileMenu getSaveMenu(FileMenu saveMenu){return saveMenu;}//Fetching SaveMenu in App
         public void setsaveMenu(FileMenu SaveMenu){return SaveMenu;}//Binding SaveMenu in App
@@ -17,13 +34,5 @@ public class MainMenuScreen extends SimpleApplication{
             filemenu.getItems().addAll(openMenu,SaveMenu,exitMenu);
         public String getOpenMenu(String openMenu){return openMenu;}
     }
-    public String EditMenu(String[] args){
-        EditMenu editMenu=new EditMenu();
 
-        EditMenu soundMenu=new FileMenu("soundMenu");
-        EditMenu VideoMenu=new FileMenu("=VideoMenu");
-        EditMenu OptionsMenu=new FileMenu("OptionsMenu");
-
-        filemenu.getItems().addAll(editMenu,soundMenu,VideoMenu,OptionsMenu);
-    }
 }
