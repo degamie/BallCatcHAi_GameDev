d0a412c636cfabfb9c4b8f993989dc83d585e4ce
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-12 22:28:04 +0530

    Add files via upload

--- src/MainApp/Ai_Character.java
+++ src/MainApp/Ai_Character.java
@@ -1,5 +1,7 @@
-//WID(06/02/2026)(Sarthak Mittal)(DegamieSign)#1
+//WID(12/02/2026)(Sarthak Mittal)(DegamieSign)#1
 public class Ai_Character {
+    public Ai_Character aiCharacter;
+    public Ai_Character getAiCharacter(Ai_Character aiCharacter){return aiCharacter;}
     public State currState;
 public void setCurrState(State currState){this.currState=currState;}
     public State getCurrState() {
