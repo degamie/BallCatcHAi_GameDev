0895a33fa7eeeaa3b8f354df97ae6f3bfa77b2e4
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-15 18:09:54 +0530

    Add files via upload

--- src/MainApp/Ai_Character.java
+++ src/MainApp/Ai_Character.java
@@ -1,9 +1,11 @@
-//WID(13/02/2026)(Sarthak Mittal)(DegamieSign)#1.1
+//WID(15/02/2026)(Sarthak Mittal)(DegamieSign)#1.1
+import javax.swing.plaf.nimbus.State;
 public class Ai_Character {
     public Ai_Character aiCharacter;
     public void setAiCharacter(Ai_Character aiCharacter){this.aiCharacter=aiCharacter;}
     public Ai_Character getAiCharacter(Ai_Character aiCharacter){return aiCharacter;}
     public State currState;
+    public State moveAiChar(Ai_Character aiCharacter,State currState){return currState+=currState.toString("MOVING AI_cHARACTER");}
 public void setCurrState(State currState){this.currState=currState;}
     public State getCurrState() {
         return currState;
