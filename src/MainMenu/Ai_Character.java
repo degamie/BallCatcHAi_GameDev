a32c6e094c028b29d578dde36b11b343af64e972
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-22 23:47:34 +0530

    Add files via upload

---
+++ src/MainMenu/Ai_Character.java
@@ -0,0 +1,23 @@
+//WID(22/02/2026)(Sarthak Mittal)(DegamieSign)#1.1.1.1.1
+import javax.swing.plaf.nimbus.State;
+public class Ai_Character {
+    public Ai_Character updateByAiChar(Ai_Character aiCharacter){getAiCharacter(aiCharacter)+setAiCharacter(aiCharacter)+1;}
+    public Ai_Character aiCharacter;
+    public void setAiCharacter(Ai_Character aiCharacter){this.aiCharacter=aiCharacter;}
+    public Ai_Character getAiCharacter(Ai_Character aiCharacter){return aiCharacter;}
+    public State currState;
+    public State moveAiChar(Ai_Character aiCharacter,State currState){return currState+=currState.toString("MOVING AI_cHARACTER");}
+    public void updateByAiCharacter(Ai_Character aiCharacter){if(aiCharacter!=null)getAiCharacter(Ai_Character);else getAiCharacter(null);}//Chekcing Aicharacter's Existence in App
+public void setCurrState(State currState){this.currState=currState;}
+    public State getCurrState() {
+        return currState;
+    }
+
+    public Ai_Character(Ai_Character aiCharacter){
+        this.aiCharacter=aiCharacter;
+        this.currState=currState.IDKE;
+    }
+    public void Upoate*(){
+        if(currState==currState.MOVING)
+    }
+}
