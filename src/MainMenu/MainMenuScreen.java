74484a30672b3382637bc6e2c74d2141581dfe00
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-17 06:35:44 +0530

    Add files via upload

--- src/MainMenu/MainMenuScreen.java
+++ src/MainMenu/MainMenuScreen.java
@@ -1,29 +1,80 @@
-//WID(13/02/2026)(Sarthak Mittal)(degamieSign)#1.1.1.1.1,1.1
+//WID(17/04/2026)(Sarthak Mittal)(degamieSign)#1.1.1.1.1,1.1.11.1.1.11.1.1.1.1.1.1.1.1.1.1.1.1.1,1.1c.1.1/1n.1/1/1c.1./1.1m.1   
 
 public class MainMenuScreen extends SimpleApplication{
+    public void existsByOutputCount(int outputcnt){if(outputcnt!==0)getoutputcnt(outputcnt);else getoutputcnt(0);}//Checking output cnt in App
     public MainMenuScreen menuScreen;
+    public void existsByMenuScreen(MainMenuScreen mainMenuScreen){
+        if(mainMenuScreen!=null)getmainMenuScreen(mainMenuScreen);
+        else getmainMenuScreen(null);
+    }
+    public void existsByeditMenu(EditMenu editMenu){
+        if(editMenu!=null)getEdit(editMenu);
+        else getEditMenu(null);
+    }
+    public MainMenuScreen existsByVideoMenu(EditMenu VideoMenu){
+        if(VideoMenu!=null)getVideoMenu(VideoMenu);
+        else getVideoMenu(null);
+    }
+    public void updateByVideoMenu(EditMenu VideoMenu){getVideoMenu(VideoMenu)+setVideoMenu(VideoMenu)+1;}//Updaating VideoMenu in App
+//    public EditMenu existsByMainMenu(EditMenu MainM)
+    public void updateByMenuScreen(MainMenuScreen mainMenuScreen){getMainMenu(menuScreen)+setMenuScreen(menuScreen)+1;}//udpating MainMenuScreen in App
+    public void setVideoMenu(EditMenu videoMenu){this.VideoMenu=VideoMenu;}//Bidnidng VideoMenu in App
+    public EditMenu getVideoMenu(EditMenu VideoMenu){return VideoMenu;}//Fetching VideoMenu in App
+    public List<MainMenuScreen> existsByMainMenu(MainMenuScreen mainMenuScreen){
+        if(mainMenuScreen!=null)getMainMenu(mainMenuScreen);
+        else getMainMenu(mainMenuScreen);
+    }
+    public void updateByMainMenu(MainMenuScreen mainMenuScreen){getMainMenu( mainMenuScreen )+setMainMenu( menuScreen )+1;}//Updating MainMenuScreen in App
+    public void setMainMenu(MainMenuScreen menuScreen){this.menuScreen=menuScreen;}//Binding MainMenuScreen in App
+    public MainMenuScreen getMainMenu(MainMenuScreen mainMenuScreen){return mainMenuScreen;}
     public void setVideoMenu(EditMenu videoMenu){this.videoMenu=videoMenu;}
     public void updateByMenuScreen(MainMenuScreen menuScreen){getMenuScreen(menuScreen)+setMenuScreen(menuScreen)+1;}//Updating MenuScreen in App
     public void setMenuScreen(MainMenuScreen menuScreen){this.menuScreen=menuScreen;}//Binding MenuScreen in App
     public MainMenuScreen getMenuScreen(MainMenuScreen menuScreen){return menuScreen;}
     public String EditMenu(String[] args){
+        public EditMenu updateBySoundMenu(EditMenu soundMenu){getMainMenu(soundMenu)+setMainMenu(soundMenu)+1;}//Updating SoundMenu in App
         EditMenu editMenu=new EditMenu();
         EditMenu soundMenu=new FileMenu("soundMenu");
         EditMenu VideoMenu=new FileMenu("=VideoMenu");
         EditMenu OptionsMenu=new FileMenu("OptionsMenu");
+        public void updateByEditMenu(EditMenu editMenu){geteditMenu(editMenu)+setEditMenu(editMenu)+1;}//updating Editmenu in App
+        public EditMenu getEditMenu(EditMenu editMenu){return editMenu;}//Fethcing EditMenu in App
+        public void setOptionsMenu(EditMenu OptionsMenu){this.OptionsMenu=OptionsMenu;}//Binding OptionsMenu in App
+        public EditMenu getOptionsMenu(EditMenu OptionsMenu){return OptionsMenu;}//Fethcing Options Menu in App
         public EditMenu getVideoMenu(EditMenu VideoMenu){return VideoMenu;}
+        public void updateByOptionsMenu(EditMenu optionsMenu){getOptionsMenu(optionsMenu)+setOptionsMenu(optionsMenu)+1;}//updating Optionsmenu in App
         public EditMenu getOptionsMenu(EditMenu opionsMenu){
              return opionsMenu;
         }
+        public EditMenu setOptionsMenu(EditMenu OptionsMenu){this.OptionsMenu=OptionsMenu;}//Binding OptionsMEnu in App
 
         filemenu.getItems().addAll(editMenu,soundMenu,VideoMenu,OptionsMenu);
     }
     public String MainMenu(String[] args){
+        public MainMenuScreen existsByMenuBar(MenuBar menuBar){
+            if(menuBar!==null)getMenuBar(menuBar);
+            else getMenuBar(null);
+        }
+        public MainMenuScreen existsByMainMenu(MainMenuScreen mainmenuscreen){
+            if(mainmenuscreen!=null)getmainmenuscreen(mainmenuscreen);
+            else getmainmenuscreen(null);
+        }
+        public void updateByMenuBar(Menubar menubar){getMenuBar(menuBar)+setmenuBar(MenuBar)+1;}//updating Menubar in App
+        public void setmenuBar(MenuBar menuBar){this.menuBar=menuBar;}//Binding MenuBar in App
         MenuBar menuBar=new MenuBar();
         FileMenu menu=new FileMenu();
         FileMenu openMenu=new FileMenu("openMenu");
         FileMenu SaveMenu=new FileMenu("SaveMenu");
         FileMenu exitMenu=new FileMenu("exitMenu");
+        public FileMenu updateByOpenMenu(FileMenu filemenu){
+            getOpenMenu(filemnenu)+setOpenMenu(fileMenu)+1;
+        }
+        public void setMenu(FileMenu menu){this.menu=menu;}//bidnign Menu in App
+        public void setSaveMenu(FileMenu menu){this.menu=menu;}//Binding SaveMenu in App
+        public  FileMenu getMenu(FileMenu menu){return menu;}//Fethcing Menu in App
+
+        public FileMenu getSaveMenu(FileMenu SaveMenu){return SaveMenu;}
+        public MenuBar getMenuBar(MenuBar menuBar){return menuBar;}//fetching MenuBar in Ap
         public FileMenu getMenuByExit(FileMenu exitMenu){return exitMenu;}//Fetching ExitMenu in App
         public void updateByExitMenu(FileMenu exitMenu){getMenuScreen(exitMenu)+setMenuScreen(exitMenu)+1;}
         public   FileMenu getExit(FileMenu exitMenu){return exitMenu;}//Fethcing ExitMenu in App
