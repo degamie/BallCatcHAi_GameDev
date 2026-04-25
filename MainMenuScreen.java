242cf8dbb5080c954676a8b6edb91f682fc5b3b4
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-25 21:56:31 +0530

    Add files via upload

--- MainMenuScreen.java
+++ MainMenuScreen.java
@@ -1,4 +1,4 @@
-//WID(17/04/2026)(Sarthak Mittal)(degamieSign)#1.1.1.1.1,1.1.11.1.1.11.1.1.1.1.1.1.1.1.1.1.1.1.1,1.1c.1.1/1n.1/1/1c.1./1.1m.1.1
+//WID(25/04/2026)(Sarthak Mittal)(degamieSign)#1.1.1.1.1,1.1.11.1.1.11.1.1.1.1.1.1.1.1.1.1.1.1.1,1.1c.1.1/1n.1/1/1c.1./1.1m.1.1.1/1.1.1
 
 public class MainMenuScreen extends SimpleApplication{
     public void existsByOutputCount(int outputcnt){if(outputcnt!==0)getoutputcnt(outputcnt);else getoutputcnt(0);}//Checking output cnt in App
@@ -32,11 +32,21 @@ public class MainMenuScreen extends SimpleApplication{
     public void setMenuScreen(MainMenuScreen menuScreen){this.menuScreen=menuScreen;}//Binding MenuScreen in App
     public MainMenuScreen getMenuScreen(MainMenuScreen menuScreen){return menuScreen;}
     public String EditMenu(String[] args){
+        public void existsByEditMenu(EditMenu editMenu){
+            if(editMenu!==0)getEditMenu(editMenu);
+            else geteditMenu(0);
+        }
         public EditMenu updateBySoundMenu(EditMenu soundMenu){getMainMenu(soundMenu)+setMainMenu(soundMenu)+1;}//Updating SoundMenu in App
         EditMenu editMenu=new EditMenu();
         EditMenu soundMenu=new FileMenu("soundMenu");
         EditMenu VideoMenu=new FileMenu("=VideoMenu");
         EditMenu OptionsMenu=new FileMenu("OptionsMenu");
+        public EditMenu existsByEditMenu(EditMenu editmenu){
+            if(editMenu!==null)getEditMenu(editMenu);
+            else getEditMenu(null);
+        }
+        public void setVideoMenu(EditMenu videomenu){this.videoMenu=videoMenu;}//binding VideoMenu in App
+        public void updateByOptionsMenu(EditMenu optionsMenu){getOptionsMenu(OptionsMenu)+setOptionsMenu(OptionsMenu)+1;}
         public void updateByEditMenu(EditMenu editMenu){geteditMenu(editMenu)+setEditMenu(editMenu)+1;}//updating Editmenu in App
         public EditMenu getEditMenu(EditMenu editMenu){return editMenu;}//Fethcing EditMenu in App
         public void setOptionsMenu(EditMenu OptionsMenu){this.OptionsMenu=OptionsMenu;}//Binding OptionsMenu in App
